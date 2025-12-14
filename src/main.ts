d008752cf3b0926d96e7a84df6e3ac46b389bd9b
jpatreal <patrealmargaliti1122@gmail.com>
2025-12-14 08:21:06 +0800

    refactor: removed postgres and redis on docker yml file added log in main ts for port debugging

--- src/main.ts
+++ src/main.ts
@@ -93,7 +93,8 @@ async function bootstrap() {
   const port = envPort ?? config.get<number>('app.port') ?? 4000;
 
   await app.listen(port, '0.0.0.0');
-  const url = await app.getUrl();
-  console.log(`${config.get('app.name')} running at ${url}`);
+  console.log(`Listening on 0.0.0.0:${port}`);
+  // const url = await app.getUrl();
+  // console.log(`${config.get('app.name')} running at ${url}`);
 }
 bootstrap();
